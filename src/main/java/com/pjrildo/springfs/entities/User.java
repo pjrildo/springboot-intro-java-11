063e5773f4893bc9dfb987cb72feb6a33b8ec952
Rildo Alves <junior.alvesp99@gmail.com>
2020-07-19 17:30:24 -0300

    User entity and resource

---
+++ src/main/java/com/pjrildo/springfs/entities/User.java
@@ -0,0 +1,87 @@
+package com.pjrildo.springfs.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private long Id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	public User(long Id, String name, String email, String phone, String password) {
+		this.Id = Id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+	
+	public long getId() {
+		return Id;
+	}
+	
+	public void setId(long Id) {
+		this.Id = Id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (Id ^ (Id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (Id != other.Id)
+			return false;
+		return true;
+	}
+}
