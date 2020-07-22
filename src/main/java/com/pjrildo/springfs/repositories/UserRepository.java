e354768766ac5b30665e53a31486d5265d2af5af
Rildo Alves <junior.alvesp99@gmail.com>
2020-07-22 06:23:21 -0300

    Service layer, component registration

--- src/main/java/com/pjrildo/springfs/repositories/UserRepository.java
+++ src/main/java/com/pjrildo/springfs/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.pjrildo.springfs.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pjrildo.springfs.entities.User;
 
+@Repository //optional annotation, since JpaRepository is already registered
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
---
+++ src/main/java/com/pjrildo/springfs/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pjrildo.springfs.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pjrildo.springfs.entities.User;
+import com.pjrildo.springfs.repositories.UserRepository;
+
+@Service //register this class as a spring service to be injected with autowired
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll(); //service layer calls repository layer method
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); //optional "get" method returns an parameterized type object 
+	}
+}
