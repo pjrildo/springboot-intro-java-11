1beddf70e684c1d6f1a7995c7fb33dc80dedc5e0
Rildo Alves <junior.alvesp99@gmail.com>
2020-08-08 20:20:46 -0300

    User Insert and Delete

--- src/main/java/com/pjrildo/springfs/services/UserService.java
+++ src/main/java/com/pjrildo/springfs/services/UserService.java
@@ -23,4 +23,12 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get(); //optional "get" method returns an parameterized type object 
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
